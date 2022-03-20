1539691703a7664def7478d29e825ea212b762cc
Georgios Loudaros <elmathe.g@gmail.com>
2022-03-20 22:56:38 +0200

    Add files via upload

---
+++ C#/2. Handling Errors.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace BugSquasher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int number = 38498;
+
+      string dinosaur = "Barney";
+
+      double mylock = 293.000;
+
+      bool isyes = true;
+
+      string band = "The Low Anthem";
+
+    }
+  }
+}
