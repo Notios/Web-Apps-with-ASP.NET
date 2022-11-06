46ed42356306fc1dd0f383ca610dea3b4861ad19
Georgios Loudaros <elmathe.g@gmail.com>
2022-11-06 22:32:40 +0200

    Update Program.cs

--- DesignStack/DesignStack/Program.cs
+++ DesignStack/DesignStack/Program.cs
@@ -57,7 +57,7 @@ namespace DesignStack
         static void Main(string[] args)
         {
             var stack = new Stack();
-            stack.Push(null);
+            stack.Push(1);
             stack.Push(2);
             stack.Push(3);
             Console.WriteLine(stack.Pop());
